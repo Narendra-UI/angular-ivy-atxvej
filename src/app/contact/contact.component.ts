7bbfe24627e3d9a4e7675a2b876c73721bb5e795
Narendra-UI <115452548+Narendra-UI@users.noreply.github.com>
2022-10-13 00:16:40 +0530

    1 file modified

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SaleemService } from '../saleem.service';
 
-let Emp= Array<{ name: string; email: string; no: number }>;
+let Emp = [{ name: 'string' }, { email: 'string' }, { no: number }];
 
 @Component({
   selector: 'app-contact',
@@ -9,25 +9,25 @@ let Emp= Array<{ name: string; email: string; no: number }>;
   styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
-
   constructor(private serviceRef: SaleemService) {}
   employs = this.serviceRef.result;
 
   ngOnInit() {}
 }
 
-const employs=[
-  { employs: [     
-  { name: "a", email: 'a@gmail.com', no: 1 },
-  { name: "s", email: 's@gmail.com', no: 2 },
-  { name: "d", email: 'd@gmail.com', no: 3 },
-  { name: "f", email: 'f@gmail.com', no: 4 },
-  { name: "g", email: 'g@gmail.com', no: 5 },
-  { name: "h", email: 'h@gmail.com', no: 6 },
-  { name: "j", email: 'j@gmail.com', no: 7 },
-  { name: "k", email: 'k@gmail.com', no: 8 },
-  { name: "l", email: 'l@gmail.com', no: 9 },
-  { name: "p", email: 'p@gmail.com', no: 10 },
-  
-  ]}
-]
+const employs = [
+  {
+    employs: [
+      { name: 'a', email: 'a@gmail.com', no: 1 },
+      { name: 's', email: 's@gmail.com', no: 2 },
+      { name: 'd', email: 'd@gmail.com', no: 3 },
+      { name: 'f', email: 'f@gmail.com', no: 4 },
+      { name: 'g', email: 'g@gmail.com', no: 5 },
+      { name: 'h', email: 'h@gmail.com', no: 6 },
+      { name: 'j', email: 'j@gmail.com', no: 7 },
+      { name: 'k', email: 'k@gmail.com', no: 8 },
+      { name: 'l', email: 'l@gmail.com', no: 9 },
+      { name: 'p', email: 'p@gmail.com', no: 10 },
+    ],
+  },
+];
