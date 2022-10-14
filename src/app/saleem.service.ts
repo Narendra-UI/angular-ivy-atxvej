d7df702ad4e19ba57c55c09bb19114d52eb0e047
Narendra-UI <115452548+Narendra-UI@users.noreply.github.com>
2022-10-14 12:24:11 +0530

    2 files modified, 4 added

--- src/app/saleem.service.ts
+++ src/app/saleem.service.ts
@@ -4,7 +4,15 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class SaleemService {
-  result: any = [];
+  result: any = [
+    {
+      name: 'sallem',
+      contact: '9876543210',
+      aadhaar: '876543219879',
+      email: 'asdfg@gmail.com',
+      gender: 'male',
+    },
+  ];
 
   constructor() {}
 }
