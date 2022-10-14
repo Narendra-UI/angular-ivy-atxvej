8e676630bd98324ee1d6c19755b04ac753fbc72c
Narendra-UI <115452548+Narendra-UI@users.noreply.github.com>
2022-10-14 18:51:59 +0530

    3 files modified

--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -4,6 +4,7 @@ import {
   ActivatedRoute,
   ActivatedRouteSnapshot,
 } from '@angular/router';
+import { SaleemService } from '../saleem.service';
 
 @Component({
   selector: 'app-view',
@@ -11,12 +12,19 @@ import {
   styleUrls: ['./view.component.css'],
 })
 export class ViewComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private serviceRef: SaleemService
+  ) {}
 
+  emp: any = {};
   ngOnInit() {
     this.route.params.subscribe((params) => {
       console.log(params);
       console.log(params['id']);
+      this.emp = this.serviceRef.result.find(
+        (element) => element.id == params['id']
+      );
     });
   }
 }
