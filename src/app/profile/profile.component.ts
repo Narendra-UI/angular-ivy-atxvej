019b1c4ed5f06a82c517b32588057c5e5e1b85f8
Narendra-UI <115452548+Narendra-UI@users.noreply.github.com>
2022-10-19 18:37:36 +0530

    2 files modified

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -10,4 +10,27 @@ export class ProfileComponent implements OnInit {
   constructor(private serviceRef: SaleemService) {}
   data = this.serviceRef.result;
   ngOnInit() {}
+  displayStyle = 'none';
+  id: any;
+
+  openPopup(id) {
+    this.displayStyle = 'block';
+    console.log(id);
+    this.id = id;
+  }
+  closePopup() {
+    var val = this.id;
+    // var filteredObj = this.data.find(function (item, i) {
+    //   if (item.id === val) {
+    //     return i + 1;
+    //   }
+    // });
+
+    var obj = this.data.findIndex((d) => d.id == val);
+
+    this.data.splice(obj, 1);
+    console.log(this.id);
+    this.displayStyle = 'none';
+    // index
+  }
 }
